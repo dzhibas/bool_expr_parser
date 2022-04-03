544a85e22c7525671163aac6392e117516518928
Nikolajus Krauklis <nkrauklis@tesla.com>
2022-04-03 20:10:05 +0200

    first try evaluation

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,73 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
+use std::collections::HashMap;
+
+use pest::{iterators::Pairs, Parser};
 
 #[derive(Parser)]
 #[grammar = "bool_expr.pest"]
 pub struct BoolExprParser;
 
-fn main() {
+fn eval(expr: Pairs<Rule>, map: HashMap<&str, &str>) -> bool {
+    let mut output = false;
+    let mut logic_or = true;
+    for pair in expr {
+        match pair.as_rule() {
+            Rule::pair => {
+                let mut inner_rules = pair.into_inner();
+                let var = inner_rules.next().unwrap().as_str();
+                let mut inner2_rules = inner_rules.next().unwrap().into_inner();
+                let op = inner2_rules.next().unwrap().as_str();
+                let val = inner2_rules.next().unwrap().as_str();
+                println!("var {} {} {}", var, op, val);
+                if map.contains_key(var) {
+                    let v = *map.get(var).unwrap();
+                    if val == v {
+                        output = match logic_or {
+                            true => {
+                                logic_or = false;
+                                output || true
+                            }
+                            false => output && true,
+                        }
+                    } else {
+                        output = match logic_or {
+                            true => {
+                                logic_or = false;
+                                output || false
+                            }
+                            false => output && false,
+                        }
+                    }
+                }
+            }
+            Rule::EOI => (),
+            Rule::logic_op => {
+                logic_or = match pair.as_str() {
+                    "and" | "AND" => false,
+                    "or" | "OR" => true,
+                    _ => false,
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+    output
+}
 
-    let expression = r#"as22 IN (a,v,'c d',213) 
+fn main() {
+    let _expression = r#"as22 IN (a,v,'c d',213) 
     or (a!=2 and ds='seo ew') 
     OR demo in ("zom", ds, 2323) 
     and a=z 
     AND !(b=3 or b=ds)"#;
+    let expression = "countryCode = NL or a=z";
 
     let ast = BoolExprParser::parse(Rule::main, &expression).expect("Failed to parse");
     println!("Tree: {:#?}", ast);
+
+    let map = HashMap::from([("countryCode", "NL"), ("a", "z")]);
+
+    println!("Evaluated answer: {:#?}", eval(ast, map));
 }
